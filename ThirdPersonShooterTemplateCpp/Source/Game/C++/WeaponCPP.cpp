1a67daeea359265b1affe6a852b9324e1cc68e59
Menotdan <32620310+Menotdan@users.noreply.github.com>
2024-04-09 14:52:29 -0400

    Add TPS C++ example.

---
+++ ThirdPersonShooterTemplateCpp/Source/Game/C++/WeaponCPP.cpp
@@ -0,0 +1,62 @@
+﻿#include "WeaponCPP.h"
+#include "Engine/Debug/DebugLog.h"
+#include "Engine/Input/Input.h"
+#include "Engine/Engine/Time.h"
+#include "Engine/Physics/Actors/RigidBody.h"
+#include "Engine/Physics/Colliders/SphereCollider.h"
+#include "Engine/Level/Actors/StaticModel.h"
+#include "Engine/Level/Level.h"
+
+WeaponCPP::WeaponCPP(const SpawnParams& params)
+    : Script(params)
+{
+    // Enable ticking OnUpdate function
+    _tickUpdate = true;
+}
+
+void WeaponCPP::OnUpdate()
+{
+    if (!_data)
+        return;
+
+    if (Input::GetAction(TEXT("Fire")) && Time::GetGameTime() >= _timeToFire)
+    {
+        _timeToFire = Time::GetGameTime() + 1.0f / _data->FireRate;
+
+        auto bullet = New<RigidBody>();
+        bullet->SetName(TEXT("Bullet"));
+        bullet->SetStaticFlags(StaticFlags::None);
+        bullet->SetUseCCD(true);
+
+        auto model = New<StaticModel>();
+        model->Model = _data->BulletModel;
+        model->SetParent(bullet);
+        model->SetStaticFlags(StaticFlags::None);
+
+        // Ideally you would also use raycasting to determine if something was hit
+        auto collider = New<SphereCollider>();
+        collider->SetParent(bullet);
+        collider->SetStaticFlags(StaticFlags::None);
+
+        bullet->SetTransform(Transform(BulletSpawnPoint->GetPosition(), Quaternion::Identity, _data->BulletScale));
+        Level::SpawnActor(bullet);
+
+        bullet->SetLinearVelocity(BulletSpawnPoint->GetDirection() * _data->BulletVelocity);
+
+        bullet->DeleteObject(_data->BulletLifetime);
+    }
+}
+
+void WeaponCPP::OnStart()
+{
+    if(WeaponSettings == nullptr)
+    {
+        DebugLog::LogError(TEXT("No settings for the weapon were found"));
+        return;
+    }
+
+    if (!WeaponSettings->WaitForLoaded())
+    {
+        _data = WeaponSettings->GetInstance<WeaponDataCPP>();
+    }
+}
---
+++ ThirdPersonShooterTemplateCpp/Source/Game/C++/WeaponDataCPP.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "Engine/Core/ISerializable.h"
+#include "Engine/Core/Types/BaseTypes.h"
+#include "Engine/Content/Assets/Model.h"
+#include "Engine/Scripting/ScriptingType.h"
+
+/// <summary>
+/// WeaponDataCPP Json Asset. 
+/// </summary>
+API_CLASS() class GAME_API WeaponDataCPP : public ISerializable
+{
+    API_AUTO_SERIALIZATION();
+    DECLARE_SCRIPTING_TYPE_NO_SPAWN(WeaponDataCPP);
+public:
+    API_FIELD(Attributes = "Range(0, 20), EditorOrder(0), EditorDisplay(\"Weapon Data\")")
+    float FireRate = 20;
+
+    API_FIELD(Attributes = "Range(1000, 10000), EditorOrder(1), EditorDisplay(\"Weapon Data\")")
+    float BulletVelocity = 1000;
+
+    API_FIELD(Attributes = "Limit(0, 20), EditorOrder(2), EditorDisplay(\"Weapon Data\")")
+    int BulletLifetime = 10;
+
+    API_FIELD(Attributes="EditorOrder(3), EditorDisplay(\"Weapon Data\")")
+    AssetReference<Model> BulletModel;
+
+    API_FIELD(Attributes = "EditorOrder(3), EditorDisplay(\"Weapon Data\")")
+    Vector3 BulletScale = Vector3(0.1f);
+};
